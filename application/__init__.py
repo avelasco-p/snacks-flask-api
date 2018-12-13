bc3ddf36d4aa46e49f4dfc78c55184faa161d87c
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-12-13 15:47:21 -0600

    added flask_login library updated application config

--- application/__init__.py
+++ application/__init__.py
@@ -8,11 +8,9 @@ def create_app(test_config=None):
     # create and configure flask app
 
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY=os.urandom(16),
-        DATABASE=os.path.join(app.instance_path, 'snacks-store.sqlite'),
-        SQLALCHEMY_DATABASE_URI='sqlite://'
-    )
+
+    app.config.from_object('config')
+    app.config.from_pyfile('config.py')
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
---
+++ application/config.py
@@ -0,0 +1,4 @@
+DEBUG = False
+SECRET_KEY = 'dev'
+SQL_ALCHEMY_ECHO = False
+SQLALCHEMY_DATABASE_URI = 'sqlite:///tmp/snacks-store.db'
