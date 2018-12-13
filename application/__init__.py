6d86701de188534fbcda4fa0b7148d46ffc90006
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-12-13 15:47:52 -0600

    changed app config import

--- application/__init__.py
+++ application/__init__.py
@@ -10,7 +10,6 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_object('config')
-    app.config.from_pyfile('config.py')
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
