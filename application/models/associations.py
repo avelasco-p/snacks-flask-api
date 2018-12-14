606f9bf100d0de90b43b1028f1ac535c5bb1d797
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-12-13 23:17:07 -0600

    fixed reference to db model

--- application/models/associations.py
+++ application/models/associations.py
@@ -2,6 +2,6 @@ from .. import db
 from . import user, product
 
 products_likes = db.Table('product_x_user',
-    db.Column('user_id', db.Integer, db.ForeignKey('users._id')),
-    db.Column('product_id', db.Integer, db.ForeignKey('products._id'))
+    db.Column('user_id', db.Integer, db.ForeignKey('users._id'), primary_key = True),
+    db.Column('product_id', db.Integer, db.ForeignKey('products._id'), primary_key = True)
 )
