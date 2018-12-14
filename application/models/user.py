9898ee7c38506c43a074fa2e2955f6b587e87698
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-12-13 21:52:47 -0600

    added table name to models

--- application/models/user.py
+++ application/models/user.py
@@ -1,15 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from .. import db
+from . import product
+
 
 class User(db.Model):
     """User schema """
+    __tablename__ = 'users'
     _id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    password = db.Column(db.String(50))
-    isAdmin = db.Column(db.Boolean)
-
-    def __init__(self, name, password, isAdmin=False):
-        self.name = name;
-        self.password = generate_password_hash(password)
-        self.isAdmin = isAdmin
-
+    name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    isAdmin = db.Column(db.Boolean, nullable=False, default=False)
