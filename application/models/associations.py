ef7dfae38030ccce96f5110dfd37b0a360d1c13e
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-12-13 22:01:42 -0600

    association table between user and products liked definition

---
+++ application/models/associations.py
@@ -0,0 +1,7 @@
+from .. import db
+from . import user, product
+
+products_likes = db.Table('product_x_user',
+    db.Column('user_id', db.Integer, db.ForeignKey('users._id')),
+    db.Column('product_id', db.Integer, db.ForeignKey('products._id'))
+)
