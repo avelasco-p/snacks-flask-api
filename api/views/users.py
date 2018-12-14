58c21902937c81ba307dcab9271c372bee6bb2ab
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-12-14 03:25:24 -0600

    added patch and get users routes still error in patch

--- api/views/users.py
+++ api/views/users.py
@@ -11,8 +11,7 @@ bp = Blueprint('users', __name__, url_prefix='/users')
 
 @bp.route('/')
 @token_required
-@admin_required
-def get_all_users(admin, current_user):
+def get_all_users(current_user):
     users = User.query.all()
 
     lusers = []
@@ -26,10 +25,11 @@ def get_all_users(admin, current_user):
 
         lusers.append(user_data)
 
-    return jsonify({'users': lusers})
+    return jsonify({'users': lusers}), 200
 
 @bp.route('/<user_public_id>')
 @token_required
+@admin_required
 def get_user_by_public_id(current_user, user_public_id):
 
     user = User.query.filter_by(public_id=user_public_id).first()
@@ -45,4 +45,42 @@ def get_user_by_public_id(current_user, user_public_id):
         return jsonify({"user" : user_data})
 
 
-    return jsonify({"message" : "user with public id: {0} not found".format(user_public_id)})
+    return jsonify({"message" : "user with public id: {0} not found".format(user_public_id)}), 404
+
+@bp.route('/<user_public_id>', methods=["PATCH"])
+@token_required
+@admin_required
+def update_user(current_user, user_public_id):
+
+    data = request.get_json()
+
+    user = User.query.filter_by(public_id=user_public_id).first()
+
+    if user and data:
+        try:
+            username = data.get('username', None)
+            password = data.get('password', None)
+            admin = data.get('admin', None)
+            products_liked = data.get('products_liked', None)
+
+            if password:
+                hashed_password = generate_password_hash(password, method='sha256')
+                
+            user.username = username if username else user.username
+            user.password = hashed_password if password else user.password
+            user.isAdmin = admin if admin else user.isAdmin
+            user.products_liked = products_liked if products_liked else user.products_liked
+
+
+            user_obj = {}
+            user_obj['admin'] = user.isAdmin
+
+            db.session.commit()
+
+            return jsonify({"message" : "user {0} updated".format(user_public_id),
+                            "data": user_obj}), 200
+        except Exception as e:
+            print('couldnt update')
+            print(e)
+
+    return jsonify({"message" : "user with public id: {0} not found".format(user_public_id)}), 404
