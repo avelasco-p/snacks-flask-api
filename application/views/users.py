919c8f60da03031fef77509a8aa9e7a6830b4fc7
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-12-13 23:23:58 -0600

    added get all users

--- application/views/users.py
+++ application/views/users.py
@@ -6,3 +6,20 @@ from ..models.user import User
 from .. import db
 
 bp = Blueprint('users', __name__, url_prefix='/users')
+
+@bp.route('/', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+
+    output = []
+
+    for user in users:
+        user_data = {}
+        user_data['id'] = user._id
+        user_data['name'] = user.name
+        user_data['isAdmin'] = user.isAdmin
+        user_data['products_liked'] = user.product_likes
+
+        output.append(user_data)
+
+    return jsonify({'users': output})
