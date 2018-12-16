fe9176faf1abc8c7c929f817aba36f1353b458fe
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-12-16 05:38:35 -0600

    changed default value from qty bought in association to 1

--- api/models/associations.py
+++ api/models/associations.py
@@ -9,5 +9,5 @@ products_likes = db.Table('product_x_user',
 products_bought = db.Table('user_shopping',
     db.Column('user_id', db.Integer, db.ForeignKey('users._id'), primary_key=True),
     db.Column('product_id', db.Integer, db.ForeignKey('products._id'), primary_key=True),
-    db.Column('product_qty', db.Integer, nullable=False, default=0)
+    db.Column('product_qty', db.Integer, nullable=False, default=1)
 )
