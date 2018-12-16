2a83b201782fc0b3d25ada4bb5b24b2efcabffc3
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-12-16 06:13:44 -0600

    added multi-likes endpoint to api/likes

--- api/views/likes.py
+++ api/views/likes.py
@@ -10,6 +10,33 @@ from .. import db
 #creating blueprint
 bp = Blueprint('likes', __name__, url_prefix='/api/likes')
 
+@bp.route('', methods=['PUT', 'PATCH'])
+@token_required
+def buy_products(current_user):
+
+    data = request.get_json()
+
+    products_liked = data.get('products', [])
+
+    if not products_liked:
+        return jsonify({'message' : 'The JSON is invalid'}), 400
+
+    for product_liked in products_liked:
+        product_p_id = product_liked.get('public_id', None)
+
+        if product_p_id:
+            product = Product.query.filter_by(public_id=product_p_id).first()
+
+            if product not in current_user.products_liked:
+                product.popularity += 1
+                current_user.products_liked.append(product)
+
+                db.session.commit()
+        else:
+            return jsonify({'message': 'The JSON is invalid'}), 400
+        
+    return jsonify({'message' : 'products liked successfuly'}), 200
+
 
 @bp.route('/<public_product_id>', methods=['PUT', 'PATCH'])
 @token_required
@@ -19,10 +46,11 @@ def like_one_product(current_user, public_product_id):
 
     if product not in current_user.products_liked:
         #adding product to list of products_liked
-        current_user.products_liked.append(product)
         product.popularity += 1
+        current_user.products_liked.append(product)
+
         db.session.commit()
 
         return jsonify({'message' : 'product liked'}), 200
 
-    return jsonify({'message' : 'product already liked'}), 200
+    return jsonify({'message' : 'product already liked'}), 304
