e0f28be272b88e09869278d2fa1d5b293862ad2c
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-12-18 10:57:33 -0600

    added date to purchase

--- api/models/associations.py
+++ api/models/associations.py
@@ -1,6 +1,8 @@
 from .. import db
 from . import user, product
 
+from datetime import datetime
+
 products_likes = db.Table('product_x_user',
     db.Column('user_id', db.Integer, db.ForeignKey('users._id')),
     db.Column('product_id', db.Integer, db.ForeignKey('products._id'))
@@ -9,5 +11,6 @@ products_likes = db.Table('product_x_user',
 products_bought = db.Table('user_shopping',
     db.Column('user_id', db.Integer, db.ForeignKey('users._id')),
     db.Column('product_id', db.Integer, db.ForeignKey('products._id')),
-    db.Column('product_qty', db.Integer, nullable=False, default=1)
+    db.Column('product_qty', db.Integer, nullable=False, default=1),
+    db.Column('purchase_date', db.DateTime, nullable=False, default=datetime.utcnow)
 )
